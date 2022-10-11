d7c0b2381d7edb0a64028c0c55bbc3a7d4d8a1c1
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-10-11 17:50:48 +0200

    done what I can of Iteration9

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,51 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1; 
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 10);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 6000);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime / 100;
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return this.currentTime % 10;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const valueAsAString = value.toString();
+      if (valueAsAString.length === 1) {
+      return "0"+valueAsAString
+    } else {
+      return valueAsAString;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const mins = this.computeTwoDigitNumber(this.getMinutes());
+    const secs = this.computeTwoDigitNumber(this.getSeconds());
+    const centi = this.computeTwoDigitNumber(this.getCentiseconds());
+    return `${mins}:${secs}.${centi}`
   }
 }
